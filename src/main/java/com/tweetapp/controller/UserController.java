3843ab78f28192cdb5c948a2ece5be2822339d19
AmithSAI12-dev <73217440+AmithSAI12-dev@users.noreply.github.com>
2022-04-07 10:36:40 +0530

    Update UserController.java

--- src/main/java/com/tweetapp/controller/UserController.java
+++ src/main/java/com/tweetapp/controller/UserController.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@CrossOrigin(origins = "http://54.84.169.248:3000")
+@CrossOrigin(origins = "http://34.234.76.96:3000")
 public class UserController {
 
     private final UserService userService;
